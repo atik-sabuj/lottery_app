28d9198f8dd57511e3d94293026bc45ada09a46d
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-17 02:57:28 +0600

    Starting Lottery App UI

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,35 +1,25 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-   HomePage({Key? key}) : super(key: key);
+   const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-   int x = 0;
 
   @override
   Widget build(BuildContext context) {
-    print('rebuild');
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('App'),
+          title: const Text('Lottery App'),
         ),
-        body: SafeArea(
-          child: Center(child: Text(x.toString(), style: TextStyle(fontSize: 50),)),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: (){
-            x++;
-            setState(() {
+        body: Column(
+          children: [
 
-            });
-            print(x.toString());
-          },
-          child: Icon(Icons.add),
+          ],
         ),
       ),
     );
