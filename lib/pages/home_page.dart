8ae15f4244ed76cacd7f16b4c7453c5ebf8539f5
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-17 04:04:23 +0600

    Lottery App Done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,7 +10,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   Random random = Random();
-  int x = 8;
+  int x = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -23,24 +23,36 @@ class _HomePageState extends State<HomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Center(child: Text('Lottery Winning Number is $x')),
+            Center(child: Text('Lottery Winning Number is 4')),
             SizedBox(height: 20,),
             Container(
-              height: 250,
+              height: x == 4 ? 600 : 250,
+              width: 300,
               decoration: BoxDecoration(
-                color: Colors.grey.withOpacity(.2),
+                color: x == 4 ? Colors.teal : Colors.grey.withOpacity(.2),
                 borderRadius: BorderRadius.circular(10),
               ),
               child: Padding(
                 padding: const EdgeInsets.all(15.0),
-                child: Column(
+                child: x == 4 ?
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(Icons.done_all,
+                      color: Colors.green, size: 35,),
+                    SizedBox(height: 15,),
+                    Text('Congratulations You Have Won the Lottery, Your Number is $x', textAlign: TextAlign.center,),
+                  ],
+                ) :
+                Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     Icon(Icons.error,
                       color: Colors.red, size: 35,),
                     SizedBox(height: 15,),
-                    Text('Better Luck Next Time Your Number is $x\ntry again',
+                    Text('Better Luck Next Time Your Number is $x\nTry Again',
                       textAlign: TextAlign.center,),
                   ],
                 ),
@@ -50,7 +62,9 @@ class _HomePageState extends State<HomePage> {
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: (){
-            print('tap');
+            x = random.nextInt(6);
+            print(x);
+            setState(() {});
           },
           child: Icon(Icons.refresh),
         ),
