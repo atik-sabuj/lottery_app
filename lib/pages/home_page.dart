d277fea8af0ccff91132bd01464b3dec2656fc16
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-17 00:50:34 +0600

    Updated body

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,16 +3,23 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  final int x = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: const Text('App'),
+        ),
         body: SafeArea(
-          child: Column(
-            children: [
-
-            ],
-          ),
+          child: Center(child: Text(x.toString(), style: TextStyle(fontSize: 50),)),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            print('tap');
+          },
+          child: Icon(Icons.add),
         ),
       ),
     );
